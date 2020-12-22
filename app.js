07a60f69217ebf3773214121084a06b29b55e707
Jang JunHee <lyradortiz913@naver.com>
2020-12-22 16:22:49 +0900

    2Dcontext

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
 const canvas = document.getElementById(canvas);
-
+const ctx = canvas.getContext("2d");
 let painting = false;
+
+ctx.strokeSytle = "black";
+ctx.lineWidth = 2.5;
 
 function stopPainting(){
     painting = false;
+}
+
+function startPainting(){
+    painting = true;
 }
 
 function onMouseMove(event){
     //console.log(event);
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    }
+    else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event){
     painting = true;
 }
 
-function onMouseUp(event){
-    stopPainting();
-}
-
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedonw",onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedonw",startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
 }
